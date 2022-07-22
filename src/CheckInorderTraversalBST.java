b2a0638e10d46e7fef579fec3648dd7898318b78
Kunal Kheeva <98793512+kunalkheeva1@users.noreply.github.com>
2022-07-22 02:06:29 -0700

    annotated it with the comments for the future references

--- src/CheckInorderTraversalBST.java
+++ src/CheckInorderTraversalBST.java
@@ -1,11 +1,16 @@
 public class CheckInorderTraversalBST {
 
+    //property of inorder is that the array contained is in ascending order
     static int checkInorderTraversalBST(int arr[], int n){
+        //run a loop from 1 instead of 0 as if its get started
+        // from 0 we cannot compare it with the previous element
         for(int i=1; i<n; i++){
+
+            //make sure they are not equal either
             if(arr[i]<= arr[i-1]){
                 return 0;
             }
-        }return 1;
+        }return 1;          //else return 1
     }
 
 
