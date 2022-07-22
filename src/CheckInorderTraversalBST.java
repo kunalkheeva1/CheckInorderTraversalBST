96c438c9108aade388c838077e6c29f7f1edb026
Kunal Kheeva <98793512+kunalkheeva1@users.noreply.github.com>
2022-07-22 02:04:04 -0700

    coded a method to check if the elements in a given array can form Inorder traversal BST or not, if yes return 1, and if not then return 0

--- src/CheckInorderTraversalBST.java
+++ src/CheckInorderTraversalBST.java
@@ -1,5 +1,17 @@
 public class CheckInorderTraversalBST {
+
+    static int checkInorderTraversalBST(int arr[], int n){
+        for(int i=1; i<n; i++){
+            if(arr[i]<= arr[i-1]){
+                return 0;
+            }
+        }return 1;
+    }
+
+
+
+
     public static void main(String[] args) {
-        
+
     }
 }
